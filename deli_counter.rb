12e7870fa626176f9c416d11f4a1be30db27e63e
Max <maxhroberts@gmail.com>
2018-10-28 14:27:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,11 +17,9 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     message="The line is currently:"
-
     katz_deli.each_with_index { |value, index|
       message += " #{index.to_i+1}. #{value}"
     }
-
     puts "#{message}"
   end
 
