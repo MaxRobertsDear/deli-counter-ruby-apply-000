9095e2d538c9707d78e9ab32764d7f916ed2eddc
Max <maxhroberts@gmail.com>
2018-10-28 14:05:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,15 @@
 # Write your code here.
 katz_deli = []
 
+
+def take_a_number ( katz_deli, name )
+    line_length = katz_deli.length + 1 
+    katz_deli.each << name
+    puts "Welcome, #{name}. You are number #{line_length} in line."
+end
+
+
+
 def line ( katz_deli )
   if katz_deli == 0 
     puts "The line is currently empty."
@@ -13,11 +22,7 @@ def line ( katz_deli )
   end
 end
   
-def take_a_number ( katz_deli, name )
-    line_length = katz_deli.length + 1 
-    katz_deli.each << name
-    puts "Welcome, #{name}. You are number #{line_length} in line."
-end
+
 
 def now_serving ( katz_deli )
   being_served = katz_deli.shift
