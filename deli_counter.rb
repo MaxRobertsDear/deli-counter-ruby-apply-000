52b334e213060e8217d56eb654a404dcbafece38
Max <maxhroberts@gmail.com>
2018-10-28 14:00:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,6 +23,6 @@ def now_serving ( katz_deli )
   if katz_deli.length == 0 
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{being_served}"
+    puts "Currently serving #{being_served}."
   end
 end
